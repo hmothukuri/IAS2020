a2901991db24dd995160760a78f24da1d1651e71
hmothukuri <63357315+hmothukuri@users.noreply.github.com>
2020-08-14 23:51:01 +0200

    Update GlobalVars.cs

--- scripts/GlobalVars.cs
+++ scripts/GlobalVars.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GlobalVars  
 {
+    //public variable to use when changing the scene
     public static string selectedAnimal;
-    // Start is called before the first frame update
+   
   
 }
