1367d50df3cded49a1b0df89bd10835d1726ab5d
hmothukuri <63357315+hmothukuri@users.noreply.github.com>
2020-08-14 23:53:18 +0200

    Update GoToPark.cs

--- scripts/GoToPark.cs
+++ scripts/GoToPark.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class GoToPark : MonoBehaviour
 {
-    private GameObject dd;
+   //creating dropdown object 
+   private GameObject dd;
    
     public TextMeshProUGUI txtObject;
    
     // Start is called before the first frame update
     void Start()
     {
+    //assigning the variables to dropdown menu 
         dd = GameObject.FindGameObjectWithTag("dropdown");
         txtObject = dd.GetComponent<TextMeshProUGUI>();
     }
