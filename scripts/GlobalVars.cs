877611f2a442ed2c9839327a5de4e1eda64897da
hmothukuri <63357315+hmothukuri@users.noreply.github.com>
2020-08-14 21:20:36 +0200

    Add files via upload

---
+++ scripts/GlobalVars.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars  
+{
+    public static string selectedAnimal;
+    // Start is called before the first frame update
+  
+}
---
+++ scripts/GoToPark.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToPark : MonoBehaviour
+{
+    private GameObject dd;
+   
+    public TextMeshProUGUI txtObject;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        dd = GameObject.FindGameObjectWithTag("dropdown");
+        txtObject = dd.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GlobalVars.selectedAnimal = txtObject.text;
+
+    }
+
+
+    
+}
